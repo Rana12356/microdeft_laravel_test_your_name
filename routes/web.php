6c0d91319f73bf3a7b448ff5f092374d4f4483f0
Rana12356 <rana15-12356@diu.edu.bd>
2023-06-03 13:02:40 +0600

    laravel jetstream installed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/product/manage', [ProductController::class, 'manage'])->name('produ
 Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
 Route::post('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
 Route::get('/product/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
